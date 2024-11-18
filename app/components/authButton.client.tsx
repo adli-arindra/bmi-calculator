0c838731fa5becedbba58e7e2b2e3ae328e2df5d
Adli Arindra <adliarindra27@gmail.com>
2024-11-18 14:22:36 +0700

    signout change

--- app/components/authButton.client.tsx
+++ app/components/authButton.client.tsx
@@ -1,20 +1,18 @@
 "use client";
 import { useSession } from "next-auth/react";
 
-import { signIn, signOut } from "@/app/auth/helpers";
+import { signIn } from "@/app/auth/helpers";
+import Link from "next/link";
 
 export default function AuthButton() {
   const session = useSession();
 
   return session?.data?.user ? (
-    <button className = "btn btn-primary text-white mx-1 rounded-full hover:btn-secondary hover:text-white transition duration-300"
-      onClick={async () => {
-        await signOut();
-        await signIn();
-      }}
+    <Link className = "btn btn-primary text-white mx-1 rounded-full hover:btn-secondary hover:text-white transition duration-300"
+      href="/api/auth/signout"
     >
       {session.data?.user?.name} : Sign Out
-    </button>
+    </Link>
   ) : (
     <button className = "btn btn-primary text-white mx-1 rounded-full hover:btn-secondary hover:text-white transition duration-300" onClick={async () => await signIn()}>Sign In</button>
   );
